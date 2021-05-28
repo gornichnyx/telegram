5f3a31a5357a1bf6f0f711b9f893a64f381b3170
gornichnyx <wsxzaqplmko@gmail.com>
2021-05-28 13:09:57 +0300

    added small search by movie name

--- KinoBot.py
+++ KinoBot.py
@@ -3,10 +3,13 @@ from telebot import types
 import config
 import tmdbsimple as tmdb
 import requests
+import json
+import requests
 tmdb.REQUESTS_SESSION = requests.Session()
-tmdb.API_KEY = '89b7322e6a500625af7a481b32251e9d'
+tmdb.API_KEY = config.TOKENTMDB
+search = tmdb.Search()
 
-bot = telebot.TeleBot(config.TOKEN)
+bot = telebot.TeleBot(config.TOKENTELEBOT)
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -22,15 +25,32 @@ def welcome(message):
 @bot.message_handler(content_types=['text'])
 def text(message):
     if message.chat.type == 'private':
-        movie = tmdb.Movies(769694)
-        response = movie.info()
-        ttt = '[{}](https://www.themoviedb.org/movie/769694-russian-gay-dude)'.format(movie.title)
         if message.text == "Я примерно знаю, что хочу посмотреть":
 
             bot.send_message(message.chat.id, ttt, parse_mode='MarkdownV2')
         elif message.text == "Рандомный фильм":
+            movie = tmdb.Movies(769694)
+            response = movie.info()
+            bot.send_message(message.chat.id, '[{}](https://www.themoviedb.org/movie/769694-russian-gay-dude)'.format(movie.title), parse_mode='MarkdownV2')
 
-            bot.send_message(message.chat.id, ttt, parse_mode='MarkdownV2')
+
+def search_by_film_name(message):
+    response = search.movie(query=message.text)
+    print(search.results)
+    for s in search.results:
+        bot.reply_to(message, s['title'])
+
+
+
+def genres(message):
+    try:
+        genres_list = message.text.split(', ')
+        for genre in genres_list:
+            bot.reply_to(message, genre)
+    except Exception as e:
+        bot.reply_to(message, 'Неправильный ввод, попробуйте снова, через запятую.')
+
+
 
 
 # run
